cdcebfa22b9f20277aa4fa3e74289fedcfff1d6b
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-27 18:42:00 +0300

    step9: add date2 to the isHoliday function

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -76,7 +76,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && isMonday(date2) && !isHoliday(date)) {
+    if (date2 && isMonday(date2) && !isHoliday(date, date2)) {
       reduction = 35;
     }
     return reduction;
@@ -86,7 +86,7 @@ function createApp(database) {
     return date.dayOfWeek == 1;
   }
 
-  function isHoliday(date) {
+  function isHoliday(date, date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
