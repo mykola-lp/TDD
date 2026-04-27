9319712a487e15537814058c6254566cf65fd2c0
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-27 20:20:07 +0300

    step17: refactor calculateCost to use PlainDate only

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -27,7 +27,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost, convert(date));
+    const cost = calculateCost(age, type, convert(date), baseCost);
     res.json({ cost });
   });
 
@@ -37,11 +37,11 @@ function createApp(database) {
     }
   }
 
-  function calculateCost(age, type, date, baseCost, date1) {
+  function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date1, baseCost);
+      return calculateCostForDayTicket(age, date, baseCost);
     }
   }
 
