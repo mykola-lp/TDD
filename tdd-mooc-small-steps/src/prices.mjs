a14cafa54b27620b33c7c780a78b381effa45f86
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-27 19:56:29 +0300

    step9: call isMonday with convert(date)

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -77,14 +77,14 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(date) && !isHoliday(convert(date))) {
+    if (date && isMonday(date, convert(date)) && !isHoliday(convert(date))) {
       reduction = 35;
     }
     return reduction;
   }
 
-  function isMonday(date) {
-    return date.getUTCDay() === 1 && convert(date).dayOfWeek === 1;
+  function isMonday(date, convert1) {
+    return convert1.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
