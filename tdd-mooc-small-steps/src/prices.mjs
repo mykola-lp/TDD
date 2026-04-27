fc6903842d6816de6d2212125546862e16c61d76
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-27 20:24:05 +0300

    step18: convert parsed Date in parseDate

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -27,13 +27,13 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const cost = calculateCost(age, type, convert(date), baseCost);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return convert(new Date(dateString));
     }
   }
 
