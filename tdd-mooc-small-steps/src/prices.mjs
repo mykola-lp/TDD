79aa8976ec8b89ed93e1d1b6d5611768fc5ef9a1
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-27 20:08:02 +0300

    step13: refactor calculateReduction to use PlainDate only

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -59,7 +59,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost) {
-    let reduction = calculateReduction(date, convert(date));
+    let reduction = calculateReduction(convert(date));
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -75,9 +75,9 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date, date1) {
+  function calculateReduction(date) {
     let reduction = 0;
-    if (date1 && isMonday(date1) && !isHoliday(date1)) {
+    if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
