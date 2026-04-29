4864eb12e96199f0d39c5ed9586f4edfd912e9cd
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-29 17:36:33 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -88,8 +88,8 @@ function createApp(database: Database) {
       let holiday = new Date(row.holiday);
       let holiday1 = Temporal.PlainDate.from(row.holiday);
       if (
-        date &&
         date1 &&
+        date &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
