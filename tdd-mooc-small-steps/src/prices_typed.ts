244a3cf4ed089e13b8eafd441b4967e9c7da7b2f
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-29 17:40:45 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -72,7 +72,7 @@ function createApp(database: Database) {
 
   function calculateReduction(date: Date | undefined, date1: Temporal.PlainDate | undefined) {
     let reduction = 0;
-    if (date1 && isMonday(date, date1) && !isHoliday(date1, date1)) {
+    if (date1 && isMonday(date, date1) && !isHoliday(date1)) {
       reduction = 35;
     }
     return reduction;
