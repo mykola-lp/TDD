701968ccab9d5041de72416796e32a2a5c672327
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-29 17:47:05 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -32,7 +32,7 @@ function createApp(database: Database) {
   }
   function parsePlainDate(dateString: string | undefined): Temporal.PlainDate | undefined { if (dateString) { return Temporal.PlainDate.from(dateString); } }
 
-  function calculateCost(age: number | undefined, type: string, date: Date | Temporal.PlainDate | undefined, baseCost: number) {
+  function calculateCost(age: number | undefined, type: string, date: Temporal.PlainDate | undefined, baseCost: number) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
