402e10770eeb08270042e7eb48e32879c5686025
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:38:15 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -87,7 +87,7 @@ function createApp(database: Database) {
       let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date1 &&
-        date1.equals(holiday)
+        date.equals(holiday)
       ) {
         return true;
       }
