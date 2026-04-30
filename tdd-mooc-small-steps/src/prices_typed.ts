0e2b2357e048dccb0bed0d2ccbef8319f9cc510c
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:54:44 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -21,7 +21,7 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string);
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, convert(date), baseCost);
     res.json({ cost });
   });
 
