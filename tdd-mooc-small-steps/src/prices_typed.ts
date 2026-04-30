b849f906477f76b20fbb54e789a241e6e571a79d
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:38:52 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -81,7 +81,7 @@ function createApp(database: Database) {
     return date.getUTCDay() === 1;
   }
 
-  function isHoliday(date: Date | Temporal.PlainDate | undefined) {
+  function isHoliday(date: Temporal.PlainDate | undefined) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
