b598f69740e85b7a487976a809764570d25938a6
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:59:05 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -24,7 +24,7 @@ function createApp(database: Database) {
     res.json({ cost });
   });
 
-  function parseDate(dateString: string | undefined): Date | undefined {
+  function parseDate(dateString: string | undefined): Temporal.PlainDate | undefined {
     if (dateString) {
       return Temporal.PlainDate.from(dateString);
     }
