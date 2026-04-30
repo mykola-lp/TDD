d0cb0daa7172d9656a7cefc63f3c4293d1d6b011
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:46:15 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -53,7 +53,7 @@ function createApp(database: Database) {
   }
 
   function calculateCostForDayTicket(age: number | undefined, date: Date | undefined, baseCost: number) {
-    let reduction = calculateReduction(date);
+    let reduction = calculateReduction(date, convert(date));
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
