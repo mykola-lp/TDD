44835d19026f6d60f263ff1b709cd23196ccbbe7
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:28:25 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -85,6 +85,7 @@ function createApp(database: Database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday1 = convert(holiday);
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
