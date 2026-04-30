b61648ad84c6997e93cd56ac91fe749de8f0ff6c
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:41:20 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -77,7 +77,7 @@ function createApp(database: Database) {
     return reduction;
   }
 
-  function isMonday(date: Date) {
+  function isMonday(date: Date, date1: Temporal.PlainDate) {
     return date.getUTCDay() === 1 && convert(date).dayOfWeek === 1;
   }
 
