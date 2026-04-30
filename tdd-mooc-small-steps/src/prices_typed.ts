f587a605ebfe0439ca8881326da7509bbc420e3d
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:30:46 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -90,7 +90,6 @@ function createApp(database: Database) {
       if (
         date1 &&
         date1.equals(holiday1) &&
-        date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
       ) {
