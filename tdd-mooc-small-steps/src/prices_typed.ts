bdd264db480881669f868740e3559e42cfa30b47
mykola-lp <mykola.sukhorab@gmail.com>
2026-04-30 11:50:32 +0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -35,7 +35,7 @@ function createApp(database: Database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost);
+      return calculateCostForDayTicket(age, date, baseCost, convert(date));
     }
   }
 
