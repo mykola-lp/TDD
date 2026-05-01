ba1746279f6eba31ef5267e6ad1f30ec08e068b7
mykola-lp <mykola.sukhorab@gmail.com>
2026-05-01 16:44:00 +0300

    level2: add rotation left func

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -14,6 +14,14 @@ export class RotatingShape {
     );
   }
 
+  rotateLeft() {
+    return new RotatingShape([
+      [this.grid[0][2], this.grid[1][2], this.grid[2][2]],
+      [this.grid[0][1], this.grid[1][1], this.grid[2][1]],
+      [this.grid[0][0], this.grid[1][0], this.grid[2][0]],
+    ]);
+  }
+
   rotateRight() {
     return new RotatingShape([
       [this.grid[2][0], this.grid[1][0], this.grid[0][0]],
