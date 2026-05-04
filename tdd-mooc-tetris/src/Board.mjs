56490cce3a34d081d2655385588c1269104037a0
mykola-lp <mykola.sukhorab@gmail.com>
2026-05-04 12:16:09 +0300

    level4: add getHeight method for block

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -90,4 +90,14 @@ export class Board {
     if (!cell) return ".";
     return cell;
   }
+
+  getHeight(block) {
+    const rows = this.getRows(block);
+    for (let i = rows.length - 1; i >= 0; i--) {
+      if (rows[i] !== ".".repeat(rows[i].length)) {
+        return i + 1;
+      }
+    }
+    return 0;
+  }
 }
