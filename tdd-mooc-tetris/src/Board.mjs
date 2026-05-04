0062fd04ce0b47b7ceb837b2645657fb0d889814
mykola-lp <mykola.sukhorab@gmail.com>
2026-05-04 11:48:15 +0300

    level4: add getCell method

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -74,4 +74,14 @@ export class Board {
     }
     return rows;
   }
+  getCell(rows, blockX, blockY, x, y) {
+    const rowIndex = y - blockY;
+    const columnIndex = x - blockX;
+    if (rowIndex < 0 || columnIndex < 0) return ".";
+    const row = rows[rowIndex];
+    if (!row) return ".";
+    const cell = row[columnIndex];
+    if (!cell) return ".";
+    return cell;
+  }
 }
