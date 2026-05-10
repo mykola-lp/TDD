e7a545f6381a20d2eac46a8a1eed549fb4c72c76
mykola-lp <mykola.sukhorab@gmail.com>
2026-05-10 10:53:19 +0300

    level10: inject random generator, add shuffle method

--- tdd-mooc-tetris/src/ShuffleBag.mjs
+++ tdd-mooc-tetris/src/ShuffleBag.mjs
@@ -1,14 +1,19 @@
 export class ShuffleBag {
-  constructor(items) {
+  constructor(items, random = Math.random) {
     this.original = items;
+    this.random = random;
     this.items = [...items];
   }
 
   next() {
     if (this.items.length === 0) {
-      this.items = [...this.original];
+      this.items = this.shuffle(this.original);
     }
 
     return this.items.shift();
+  }
+
+  shuffle(items) {
+    return [...items];
   }
 }
