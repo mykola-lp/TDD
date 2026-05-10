69e6b730eff8f8cb9d7007896135c2e8484077dd
mykola-lp <mykola.sukhorab@gmail.com>
2026-05-10 10:35:38 +0300

    level10: refill bag when empty

--- tdd-mooc-tetris/src/ShuffleBag.mjs
+++ tdd-mooc-tetris/src/ShuffleBag.mjs
@@ -1,9 +1,14 @@
 export class ShuffleBag {
   constructor(items) {
+    this.original = items;
     this.items = [...items];
   }
 
   next() {
+    if (this.items.length === 0) {
+      this.items = [...this.original];
+    }
+
     return this.items.shift();
   }
 }
--- tdd-mooc-tetris/test/ShuffleBag.test.mjs
+++ tdd-mooc-tetris/test/ShuffleBag.test.mjs
@@ -14,4 +14,14 @@ describe("ShuffleBag", () => {
     expect(bag.next()).to.equal("I");
     expect(bag.next()).to.equal("T");
   });
+
+  test("starts over after all items are drawn", () => {
+    const bag = new ShuffleBag(["I", "T"]);
+
+    expect(bag.next()).to.equal("I");
+    expect(bag.next()).to.equal("T");
+
+    expect(bag.next()).to.equal("I");
+    expect(bag.next()).to.equal("T");
+  });
 });
